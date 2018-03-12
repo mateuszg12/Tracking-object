ec36ccb3a62b20ee308bc38d200e3c9d8df17e90
Unknown <mgmateusz.grudzien@gmail.com>
2018-03-12 01:55:13 +0100

    Adds capturing frames from cam

--- c++/Tracking-object/main.cpp
+++ c++/Tracking-object/main.cpp
@@ -7,10 +7,40 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv_modules.hpp>
 
-using namespace std;
+#define CAM_INDEX 1
+#define FRAME_TIME 50
+#define FRAME_WIDTH 1280
+#define FRAME_HEIGHT 720
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cv::VideoCapture camera;
+    camera.open(CAM_INDEX);
+    if(!camera.isOpened())
+    {
+        std::cout << "Could not open camera on port " << CAM_INDEX << "!" << std::endl;
+        return -1;
+    }
+
+    camera.set(CV_CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
+    camera.set(CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
+
+    cv::Mat frame(FRAME_WIDTH, FRAME_HEIGHT, CV_8UC3);
+
+    cv::namedWindow("Camera", cv::WINDOW_AUTOSIZE);
+    cv::moveWindow("Camera", 0, 0);
+
+    while(true)
+    {
+        camera >> frame;
+
+        cv::imshow("Camera", frame);
+
+        char user_input = cv::waitKey(FRAME_TIME);
+        if(user_input == 27)
+            break;
+    }
+
+    camera.release();
     return 0;
 }
